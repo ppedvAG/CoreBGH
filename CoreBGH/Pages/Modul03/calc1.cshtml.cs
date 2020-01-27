b3b515af8c6bd41aff0f0fcdde27d56c340e67ca
Hannes Preishuber <hannesp@ppedv.de>
2020-01-27 14:17:58 +0100

    Calc Step 1

---
+++ CoreBGH/Pages/Modul03/calc1.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoreBGH.Pages.Modul03
+{
+    public class calc1Model : PageModel
+    {
+        public int Ergebnis { get; set; }
+        public void OnGet()
+        {
+
+        }
+        public void OnPost()
+        {
+            int eins =int.Parse(Request.Form["eins"].FirstOrDefault());
+            int zwei = int.Parse(Request.Form["zwei"].FirstOrDefault());
+            Ergebnis = eins + zwei;
+
+
+        }
+    }
+}
