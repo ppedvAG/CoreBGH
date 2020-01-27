5a2b5163954baf9cacdc3cfbc62b482f686509d9
Hannes Preishuber <hannesp@ppedv.de>
2020-01-27 14:36:29 +0100

    Page Handler mit Tag Helper

---
+++ CoreBGH/Pages/Modul03/calc2.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoreBGH.Pages.Modul03
+{
+    public class calc2Model : PageModel
+    {
+        public int Ergebnis { get; set; }
+        public void OnGet()
+        {
+
+        }
+        public void OnPostPlus()
+        {
+            int eins = int.Parse(Request.Form["eins"].FirstOrDefault());
+            int zwei = int.Parse(Request.Form["zwei"].FirstOrDefault());
+            Ergebnis = eins + zwei;
+
+
+        }
+        public void OnPostMinus()
+        {
+            int eins = int.Parse(Request.Form["eins"].FirstOrDefault());
+            int zwei = int.Parse(Request.Form["zwei"].FirstOrDefault());
+            Ergebnis = eins - zwei;
+
+
+        }
+    }
+}
