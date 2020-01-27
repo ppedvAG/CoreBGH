7bff649719e8a7789f896e19f1462516224d21bc
Hannes Preishuber <hannesp@ppedv.de>
2020-01-27 15:54:35 +0100

    Excel Export mit Page Handler

--- CoreBGH/Pages/modul04/UsePartial.cshtml.cs
+++ CoreBGH/Pages/modul04/UsePartial.cshtml.cs
@@ -10,9 +10,19 @@ namespace CoreBGH.Pages.modul04
     public class UsePartialModel : PageModel
     {
         public int MyProperty { get; set; } = 42;
+        public string[] Tage { get; set; } = new string[7];
         public void OnGet()
         {
 
+        }
+        public PartialViewResult OnGetExcel()
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                Tage[i]= Enum.GetName(typeof(DayOfWeek), i);
+        }
+            HttpContext.Response.ContentType = "application/vnd.ms-excel";
+            return Partial("_excel");
         }
     }
 }
