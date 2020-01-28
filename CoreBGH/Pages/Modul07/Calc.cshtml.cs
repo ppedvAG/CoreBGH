41f8975113e3ea6f058714b0cef9581814f2e43d
Hannes Preishuber <hannesp@ppedv.de>
2020-01-28 15:59:37 +0100

    Postzahlen Json

---
+++ CoreBGH/Pages/Modul07/Calc.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoreBGH.Pages.Modul07
+{
+    public class CalcModel : PageModel
+    {
+        [BindProperty]
+        public int Eins { get; set; }
+        [BindProperty]
+        public int Zwei { get; set; }
+        public int Ergebnis { get; set; }
+       
+        public void OnPostPlus()
+        {
+            Ergebnis = Eins + Zwei;
+        }
+        public void OnPostMinus()
+        {
+            Ergebnis = Eins - Zwei;
+        }
+    }
+}
--- CoreBGH/data/Aufgaben.cs
+++ CoreBGH/data/Aufgaben.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,11 @@ namespace CoreBGH.data
 {
     public class Aufgaben
     {
+        [Display(Description ="ID")]
         public int ID { get; set; }
+        [Display(Description = "Aufgabe",Name ="AufgabeName")]
+        [MaxLength(20,ErrorMessage ="maximal 20 Zeichen")]
+        [MinLength(2, ErrorMessage = "minaml 2 Zeichen")]
         public string Text { get; set; }
         public DateTime Datum { get; set; }
         public bool Getan { get; set; }
