0680255296b2f5bc90ebb1a9737f4ae105cfe974
Hannes Preishuber <hannesp@ppedv.de>
2020-01-28 09:56:19 +0100

    Tempdata von Page zu Page Daten

---
+++ CoreBGH/Pages/Modul05/eingabe.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoreBGH.Pages.Modul05
+{
+    public class eingabeModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+        public void OnPostTuwas([FromForm] string eins)
+        {
+            TempData.Add("hannes", eins);
+            Response.Redirect("ergebnis");
+        }
+    }
+}
