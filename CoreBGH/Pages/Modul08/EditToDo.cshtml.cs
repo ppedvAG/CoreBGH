57537d2916500630ff548ce87a87c87faf3b9945
Hannes Preishuber <hannesp@ppedv.de>
2020-01-29 13:21:50 +0100

    Formular Modelbindin Modelstate

---
+++ CoreBGH/Pages/Modul08/EditToDo.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreBGH.data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoreBGH.Pages.Modul08
+{
+    public class EditToDoModel : PageModel
+    {
+        [BindProperty]
+        public Todos MyToDo { get; set; }
+        public void OnGet()
+        {
+  //          MyToDo = new Todos();
+  //var rnd = new Random();
+  //          MyToDo.id = rnd.Next(10000); //Fake um DB identity zu simulieren
+
+        }
+        public IActionResult OnPost()
+        {
+            ModelState.Remove("id"); //akademisch -Fehler aus Modelstate entfernen
+            ModelState.Clear();
+            var rnd = new Random();
+            MyToDo.id = rnd.Next(10000); //Fake um DB identity zu simulieren
+
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("hannes", "lern mal asp.net core");
+                return Page();
+            }
+
+          
+            return Page();
+        }
+    }
+}
--- CoreBGH/data/todos.cs
+++ CoreBGH/data/todos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,7 +10,9 @@ namespace CoreBGH.data
    
     public class Todos
     {
+     
         public int userId { get; set; }
+        [Key]
         public int id { get; set; }
         public string title { get; set; }
         public bool completed { get; set; }
